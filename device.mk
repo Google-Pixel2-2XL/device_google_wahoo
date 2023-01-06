a29b1762dc190b9120187fd585ee18fcbd3b6756
Jun Wang <quic_junwan@quicinc.com>
2023-01-06 17:06:52 +0000

    wahoo: Disable Skia tracing by default
    If the prop "debug.hwui.skia_atrace_enabled" is enabled, too many
    skia tracing calls will be printed in every frame. This has obvious
    overhead on performance/power, and can cause many janks. So disable
    it by default, and user can set it manually if necessary.

--- device.mk
+++ device.mk
@@ -222,6 +222,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
    debug.sf.auto_latch_unsignaled=true
 
+# Skia tracing
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.hwui.skia_atrace_enabled=false
+
 # b/73640835
 PRODUCT_PROPERTY_OVERRIDES += \
     sdm.debug.rotator_downscale=1
